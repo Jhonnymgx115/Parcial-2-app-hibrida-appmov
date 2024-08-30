42d28c25a14d5eb87616ba8b70e75b59e1822b2f
Jorge Barreto-JBLL <86163918+Jhonnymgx115@users.noreply.github.com>
2024-08-30 09:28:45 -0500

    Update app.js
    "Añadir estructura básica del HTML y selectores del DOM para las vistas y la navegación."

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+// Elementos del DOM
+const views = document.querySelectorAll('.view');
+const navLinks = document.querySelectorAll('.navbar-menu a');
+const loginForm = document.getElementById('loginForm');
+const registerForm = document.getElementById('registerForm');
+const showRegisterBtn = document.getElementById('showRegister');
+const showLoginBtn = document.getElementById('showLogin');
+const logoutBtn = document.getElementById('logoutButton');
+const loginNavItem = document.querySelector('a[href="#login"]');
